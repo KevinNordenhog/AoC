bf392b45a6225c4b12886bf1a671d85c4d256098
KevinNordenhog <kevin.nordenhog@gmail.com>
2021-12-07 20:48:23 +0100

    Aoc Day 6 part one

---
+++ Aoc6.py
@@ -0,0 +1,32 @@
+SIZE = 1000
+
+
+def main():
+    with open("input6.txt") as file:
+        vals = list(map(int, file.read().split(',')))
+    print(partOne(vals))
+    print(partTwo(vals))
+
+
+def partOne(vals):
+    for _ in range(80):
+        zeroCnt = vals.count(0)
+        for j in range(len(vals)):
+            vals[j] = vals[j]-1 if vals[j] > 0 else 6
+            # if vals[j] == 0:
+            #     vals[j] = 6
+            # else:
+            #     vals[j] -= 1
+        for _ in range(zeroCnt):
+            vals.append(8)
+    return len(vals)
+
+
+def partTwo(vals):
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
+# P1 376194
