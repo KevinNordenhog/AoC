bd4bb44e1bd3141d9050c824f8eb9e433f5e2e97
KevinNordenhog <kevin.nordenhog@gmail.com>
2021-12-07 20:11:44 +0100

    AoC Day 5

--- Aoc5.py
+++ Aoc5.py
@@ -1,4 +1,6 @@
 SIZE = 1000
+
+
 def main():
     with open("input5.txt") as file:
         lines = file.read().splitlines()
@@ -14,42 +16,41 @@ def partOne(vals):
 
     return sum([sum(row) for row in matrix])
 
-def partTwo(vals):
-    return 0
 
-def addValuesToMatrix(val, matrix, diagonal = False):
+def partTwo(vals):
+    matrix = createMatrix()
+    for val in vals:
+        matrix = addValuesToMatrix(val, matrix, True)
+
+    return sum([sum(row) for row in matrix])
+
+
+def addValuesToMatrix(val, matrix, diagonal=False):
     x1, y1 = list(map(int, val[0].split(',')))
     x2, y2 = list(map(int, val[1].split(',')))
 
     if x1 == x2:
-        if y1 < y2:
-            for i in  range(y1,y2+1):
-                matrix[x1][i] = matrix[x1][i]+(matrix[x1][i] < 1) if matrix[x1][i] is not False else 0
-                #matrix[x1][i] += 1
-        else:
-            for i in  range(y1,y2-1,-1):
-                matrix[x1][i] = matrix[x1][i]+(matrix[x1][i] < 1) if matrix[x1][i] is not False else 0
-                #matrix[x1][i] += 1
-
+        for i in range(min(y1, y2), max(y1, y2)+1):
+            matrix[x1][i] = matrix[x1][i] + \
+                (matrix[x1][i] < 1) if matrix[x1][i] is not False else 0
     elif y1 == y2:
-        if x1 < x2:
-            for i in  range(x1,x2+1):
-                matrix[i][y1] = matrix[i][y1]+(matrix[i][y1] < 1) if matrix[i][y1] is not False else 0
-                #matrix[i][y1] += 1
-        else:
-            for i in  range(x1,x2-1, -1):
-                matrix[i][y1] = matrix[i][y1]+(matrix[i][y1] < 1) if matrix[i][y1] is not False else 0
-                #matrix[i][y1] += 1
-    # if diagonal and abs(x1-x2) == abs(y1-y2):
-    #     for i in range(abs(x1-x2)):
-    #         matrix[i][y1] = matrix[i][y1]+(matrix[i][y1] < 1) if matrix[i][y1] is not False else 0
+        for i in range(min(x1, x2), max(x1, x2)+1):
+            matrix[i][y1] = matrix[i][y1] + \
+                (matrix[i][y1] < 1) if matrix[i][y1] is not False else 0
+    elif diagonal and abs(x1-x2) == abs(y1-y2):
+        dirx = int((x2-x1)/abs(x2-x1))
+        diry = int((y2-y1)/abs(y2-y1))
+        for i in range(abs(x1-x2)+1):
+            x = x1 + (dirx*i)
+            y = y1 + (diry*i)
+            matrix[x][y] = matrix[x][y] + \
+                (matrix[x][y] < 1) if matrix[x][y] is not False else 0
     return matrix
 
+
 def createMatrix():
-    return [[False for _ in range(SIZE) ]for _ in range(SIZE)]
-    #return [[0 for _ in range(SIZE) ]for _ in range(SIZE)]
+    return [[False for _ in range(SIZE)]for _ in range(SIZE)]
+
 
 if __name__ == "__main__":
     main()
-
-#P1 8350
