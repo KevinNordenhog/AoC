5b38e6558194932679b970b46e99218cff8fb62f
Kevin Nordenhög <kevin.nordenhog@consultant.volvo.com>
2023-12-01 14:52:19 +0100

    Start of day one

---
+++ 2023/Aoc1.py
@@ -0,0 +1,49 @@
+import re
+numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+def aoc():
+    with open("input1.txt") as file:
+        lines = file.read().splitlines()
+        nums = []
+        nums2 = []
+        for line in lines:
+            # num = ''.join(char for char in line if char.isdigit())
+            # nums.append(int(num[0]+num[-1]))
+            
+            nums2.append(findDigits(line))
+        print(sum(nums))
+        for i in range(0, len(nums2)):
+            print(nums2[i])
+            print(lines[i])
+        print(sum(nums2))
+
+def findDigits(string):
+    numbersInString = [(string[i], i) for i in range(0, len(string)) if string[i].isdigit()]
+    for number in numbers:
+        if number in string:
+            numbersInString.extend([(number, m.start()) for m in re.finditer(number, string)])
+    highestNumber = ('',-1)
+    lowestNumber = ('',999)
+    for number in numbersInString:
+        i = number[1]
+        if i < lowestNumber[1]:
+            lowestNumber = number
+        if i > highestNumber[1]:
+            highestNumber = number
+    return getSum(lowestNumber, highestNumber)
+
+def getSum(firstNumber: tuple[str, int], secondNumber: tuple[str, int]):
+    if firstNumber[1] == secondNumber[1]:
+        return int(getNumber(firstNumber[0]))
+    else:
+        return int(getNumber(firstNumber[0]) + getNumber(secondNumber[0]))
+
+def getNumber(string):
+    if string.isdigit():
+        return string
+    else:
+        return str(numbers.index(string))
+
+aoc()
+#P1 54990
+#P2 51133
