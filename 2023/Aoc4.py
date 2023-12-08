dcb82b4d09cb198c2e861460bbb33e4425311784
Kevin Nordenhög <kevin.nordenhog@consultant.volvo.com>
2023-12-08 14:33:04 +0100

    Start of AoC 4

---
+++ 2023/Aoc4.py
@@ -0,0 +1,47 @@
+# As far as the Elf has been able to figure out, you have to figure out which of the numbers you have appear in the list of winning numbers. The first match makes the card worth one point and each match after the first doubles the point value of that card.
+
+# For example:
+
+# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+# In the above example, card 1 has five winning numbers (41, 48, 83, 86, and 17) and eight numbers you have (83, 86, 6, 31, 17, 9, 48, and 53). Of the numbers you have, four of them (48, 83, 17, and 86) are winning numbers! That means card 1 is worth 8 points (1 for the first match, then doubled three times for each of the three matches after the first).
+
+# Card 2 has two winning numbers (32 and 61), so it is worth 2 points.
+# Card 3 has two winning numbers (1 and 21), so it is worth 2 points.
+# Card 4 has one winning number (84), so it is worth 1 point.
+# Card 5 has no winning numbers, so it is worth no points.
+# Card 6 has no winning numbers, so it is worth no points.
+# So, in this example, the Elf's pile of scratchcards is worth 13 points.
+
+# Take a seat in the large pile of colorful cards. How many points are they worth in total?
+def main():
+    with open("input4.txt") as file:
+        lines = file.read().splitlines()
+    splitLines = [line.replace(" | ",": ").split(": ") for line in lines]
+    cards = {}
+    for card in splitLines:
+        cards[card[0][4:]] = (card[1].split(), card[2].split())
+    print("Part One:", partOne(cards))
+    print("Part Two:", partTwo(lines))
+
+def partOne(cards):
+    points = 0
+    for game in cards:
+        winningNumberCount = 0
+        for number in cards[game][0]:
+            if number in cards[game][1]:
+                winningNumberCount += 1
+        if winningNumberCount > 1:
+            points += (winningNumberCount-1) ** 2
+        else:
+            points += winningNumberCount
+    return points
+def partTwo(lines: list[list[str]]):
+    return 0
+
+if __name__ == "__main__":
+    main()
